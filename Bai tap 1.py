d613de40729dfdb1f996ba16872f9c66465e12f2
Nguyentran2610 <156576854+Nguyentran2610@users.noreply.github.com>
2024-03-10 18:57:19 +0700

    Add files via upload

---
+++ Bai tap 1.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+        self.tail = self.head
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
+
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += ' --> '
+            temp_node = temp_node.next
+        return result
+
+
+linked_list = LinkedList(10)
+linked_list.append(20)
+linked_list.append(30)
+linked_list.append(40)
+print(linked_list)
